03c0e2e9d9212cacec1c379c14975d8f60f34857
Khariza <kharizzakaye@gmail.com>
2024-10-17 14:03:55 +0800

    Added Jigsaw Inventory Project

--- src/pages/JigsawInventory.tsx
+++ src/pages/JigsawInventory.tsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import NavbarComponent from '../components/NavbarComponent'
 
-type Props = {}
+import "../styles/projects/project-main.css"
+import { Container } from 'react-bootstrap'
+import TechnologyBadgeComponent from '../components/TechnologyBadgeComponent'
+import projectsData from "../data/projects.json"
+import { useLocation } from 'react-router-dom'
+import ProjectScreenshotsComponent from '../components/ProjectScreenshotsComponent'
+import BrowseProjectsComponent from '../components/BrowseProjectsComponent'
+import BackButtonComponent from '../components/BackButtonComponent'
+import DemoButtonsComponent from '../components/DemoButtonsComponent'
 
-function JigsawInventory({}: Props) {
+const JigsawInventory = () => {
+
+  const location = useLocation();
+  const pathname = location.pathname;
+  const fetchProjectData = projectsData.data.filter(item => item.projectLink === pathname);
+  const projectData = fetchProjectData[0];
+
   return (
-    <div>JigsawInventory</div>
+    <>
+      <NavbarComponent />
+
+      <div className="project-body">
+        <Container>
+
+          <BackButtonComponent />
+
+          <h1 className="project-page-title">{projectData.title}</h1>
+          <h2 className="project-page-subtitle text-white">{projectData.subtitle}</h2>
+       
+          <TechnologyBadgeComponent technologiesList={projectData.technologies} />
+
+          <p className="project-page-description text-white">
+            An inventory system currently being used by the Jigsaw Puzzle Community PH (JPCPH) and Puzzle Mansion administrators, 
+            to track thousands of jigsaw puzzles, in an effort to take back the Guinness World Record for having the most number 
+            of jigsaw puzzles in a collection.
+          </p>
+
+          <br></br>
+          <br></br>
+
+          <DemoButtonsComponent projectData={projectData} />
+
+          <ProjectScreenshotsComponent imagesList={projectData.sampleImages} />
+
+          <BrowseProjectsComponent projectsListData={projectsData.data} />
+
+        </Container>
+      </div>
+      
+    </>
   )
 }
 
