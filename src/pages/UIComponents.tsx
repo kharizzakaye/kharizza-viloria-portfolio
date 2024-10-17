efab77111cfcd0d788f0407653ed14eb3d19a102
Khariza <kharizzakaye@gmail.com>
2024-10-17 14:22:12 +0800

    Added UI components project

--- src/pages/UIComponents.tsx
+++ src/pages/UIComponents.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import NavbarComponent from '../components/NavbarComponent'
 
-type Props = {}
+import "../styles/projects/project-main.css"
+import { Container } from 'react-bootstrap'
+import TechnologyBadgeComponent from '../components/TechnologyBadgeComponent'
+import projectsData from "../data/projects.json"
+import { useLocation } from 'react-router-dom'
+import ProjectScreenshotsComponent from '../components/ProjectScreenshotsComponent'
+import BrowseProjectsComponent from '../components/BrowseProjectsComponent'
+import BackButtonComponent from '../components/BackButtonComponent'
+import DemoButtonsComponent from '../components/DemoButtonsComponent'
 
-function UIComponents({}: Props) {
+const UIComponents = () => {
+
+  const location = useLocation();
+  const pathname = location.pathname;
+  const fetchProjectData = projectsData.data.filter(item => item.projectLink === pathname);
+  const projectData = fetchProjectData[0];
+
   return (
-    <div>UIComponents</div>
+    <>
+      <NavbarComponent />
+
+      <div className="project-body">
+        <Container>
+
+          <BackButtonComponent />
+
+          <h1 className="project-page-title">{projectData.title}</h1>
+          <h2 className="project-page-subtitle text-white">{projectData.subtitle}</h2>
+       
+          <TechnologyBadgeComponent technologiesList={projectData.technologies} />
+
+          <BrowseProjectsComponent projectsListData={projectsData.data} />
+
+        </Container>
+      </div>
+      
+    </>
   )
 }
 
