1237380a4befa8a6e7a739b7f0fdfe0a89cf92b9
Khariza <kharizzakaye@gmail.com>
2024-10-17 14:08:23 +0800

    Added Design Portfolio project

--- src/pages/DesignPortfolio.tsx
+++ src/pages/DesignPortfolio.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import NavbarComponent from '../components/NavbarComponent'
 
-type Props = {}
+import "../styles/projects/project-main.css"
+import { Container } from 'react-bootstrap'
+import TechnologyBadgeComponent from '../components/TechnologyBadgeComponent'
+import projectsData from "../data/projects.json"
+import { useLocation } from 'react-router-dom'
+import ProjectScreenshotsComponent from '../components/ProjectScreenshotsComponent'
+import BrowseProjectsComponent from '../components/BrowseProjectsComponent'
+import BackButtonComponent from '../components/BackButtonComponent'
+import DemoButtonsComponent from '../components/DemoButtonsComponent'
 
-function DesignPortfolio({}: Props) {
+const DesignPortfolio = () => {
+
+  const location = useLocation();
+  const pathname = location.pathname;
+  const fetchProjectData = projectsData.data.filter(item => item.projectLink === pathname);
+  const projectData = fetchProjectData[0];
+
   return (
-    <div>DesignPortfolio</div>
+    <>
+      <NavbarComponent />
+
+      <div className="project-body">
+        <Container>
+
+          <BackButtonComponent />
+
+          <h1 className="project-page-title">{projectData.title}</h1>
+          <h2 className="project-page-subtitle text-white">{projectData.subtitle}</h2>
+       
+          <TechnologyBadgeComponent technologiesList={projectData.technologies} />
+
+          <DemoButtonsComponent projectData={projectData} />
+
+          <ProjectScreenshotsComponent imagesList={projectData.sampleImages} />
+
+          <BrowseProjectsComponent projectsListData={projectsData.data} />
+
+        </Container>
+      </div>
+      
+    </>
   )
 }
 
