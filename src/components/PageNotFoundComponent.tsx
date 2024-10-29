2a5ba3aded8f98f728635b8176540f2e1a8af463
Khariza <kharizzakaye@gmail.com>
2024-10-29 13:50:29 +0800

    Add 404 Error page

---
+++ src/components/PageNotFoundComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import "../styles/projects/project-main.css"
+
+const PageNotFoundComponent = () => {
+  return (
+    <div className="project-body">
+        <Container>
+            Page not found
+        </Container>
+    </div>
+  )
+}
+
+export default PageNotFoundComponent
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarComponent from '../components/NavbarComponent'
+import PageNotFoundComponent from '../components/PageNotFoundComponent'
+
+const PageNotFound = () => {
+  return (
+    <>
+        <NavbarComponent />
+        <PageNotFoundComponent />
+    </>
+  )
+}
+
+export default PageNotFound
