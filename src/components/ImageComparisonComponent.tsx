2bb18b7ca9fe55c02cc3aa93a9f558be85b77f5f
Khariza <kharizzakaye@gmail.com>
2024-11-06 20:12:34 +0800

    update photos

--- src/components/ImageComparisonComponent.tsx
+++ src/components/ImageComparisonComponent.tsx
@@ -4,8 +4,7 @@ import ReactCompareImage from 'react-compare-image';
 const ImageComparisonComponent = (imagesList: any) => {
     return (
         <section>
-            {/* <Row xl={3} lg={3} md={2} sm={1} xs={1} className="g-4"> */}
-            <Row md={2} sm={1} xs={1} className="g-4">
+            <Row xl={3} lg={3} md={2} sm={1} xs={1} className="g-4">
                 { imagesList.imagesList.map((item: any, index: any) => (
                     <Col key={index}>
                         <ReactCompareImage 
