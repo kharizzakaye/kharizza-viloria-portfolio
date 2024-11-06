4b1796a60939f04797669690faf5e00fad4c760b
Khariza <kharizzakaye@gmail.com>
2024-11-06 12:49:35 +0800

    Added photo restoration project

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import UIComponents from './pages/UIComponents';
 import PetDex from './pages/PetDex';
 import GraphicDesign from './pages/GraphicDesign';
 import PageNotFound from './pages/PageNotFound';
+import PhotoRestoration from './pages/PhotoRestoration';
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
           <Route path={process.env.REACT_APP_URL_UI_COMPONENTS} element={<UIComponents />} />
           <Route path={process.env.REACT_APP_URL_PET_DEX} element={<PetDex />} />
           <Route path={process.env.REACT_APP_URL_GRAPHIC_DESIGN} element={<GraphicDesign />} />
+          <Route path={process.env.REACT_APP_URL_PHOTO_RESTORATION} element={<PhotoRestoration />} />
           <Route path="*" element={<PageNotFound /> } />
         </Routes>
       </Router>
---
+++ src/components/ImageComparisonComponent.tsx
@@ -0,0 +1,22 @@
+import { Col, Row } from 'react-bootstrap';
+import ReactCompareImage from 'react-compare-image';
+
+const ImageComparisonComponent = (imagesList: any) => {
+    return (
+        <section>
+            {/* <Row xl={3} lg={3} md={2} sm={1} xs={1} className="g-4"> */}
+            <Row md={2} sm={1} xs={1} className="g-4">
+                { imagesList.imagesList.map((item: any, index: any) => (
+                    <Col key={index}>
+                        <ReactCompareImage 
+                            leftImage={item.beforeImage.imageLink}
+                            rightImage={item.afterImage.imageLink}
+                        />;
+                    </Col>
+                ))}
+            </Row>
+        </section>
+    )
+}
+
+export default ImageComparisonComponent
