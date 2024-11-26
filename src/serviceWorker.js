c5e936448f3876b7a879fb8107e421c8288d0ea2
Khariza <kharizzakaye@gmail.com>
2024-11-26 21:43:59 +0800

    register the service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,8 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      // const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
-      const swUrl = `src/serviceWorker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/src/serviceWorker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
