b7068559ad23eb3e927ac504a62aa27fd8b41f0a
Khariza <kharizzakaye@gmail.com>
2024-12-10 15:45:47 +0800

    update footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,54 @@
-import { faGithub, faSquareBehance } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Col, Container, Row } from 'react-bootstrap'
+import React from 'react';
+import { faGithub, faSquareBehance } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Col, Container, Row, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
-function Footer() {
+const Footer: React.FC = () => {
+    const renderTooltip = (message: string) => (props: any) => (
+        <Tooltip id="icon-tooltip" {...props}>
+            {message}
+        </Tooltip>
+    );
+
+    const overlayTriggerDelay = {
+        show: 250, 
+        hide: 400
+    }
+
     return (
         <section className="footer-section">
             <Container>
                 <Row>
-                    <Col className="footer-icons">
-                        <a href="https://github.com/kharizzakaye" target="_blank">
-                            <FontAwesomeIcon icon={faGithub} className="footer-icons" />
-                        </a>
+                    <Col>
+                        <p>Visit my other profiles</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        <OverlayTrigger
+                            placement="top"
+                            delay={overlayTriggerDelay}
+                            overlay={renderTooltip('GitHub')}
+                        >
+                            <a href="https://github.com/kharizzakaye" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faGithub} className="footer-icons" />
+                            </a>
+                        </OverlayTrigger>
 
-                        <a href="https://www.behance.net/kviloria" target="_blank">
-                            <FontAwesomeIcon icon={faSquareBehance} className="footer-icons" />
-                        </a>
+                        <OverlayTrigger
+                            placement="top"
+                            delay={overlayTriggerDelay}
+                            overlay={renderTooltip('Behance')}
+                        >
+                            <a href="https://www.behance.net/kviloria" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faSquareBehance} className="footer-icons" />
+                            </a>
+                        </OverlayTrigger>
                     </Col>
                 </Row>
             </Container>
         </section>
-    )
-}
+    );
+};
 
 export default Footer;
